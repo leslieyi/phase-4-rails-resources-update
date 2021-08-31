39649003505d2d3156d3069e6771d3c9d20d005e
leslieyi <leslieyi411@gmail.com>
2021-08-30 21:51:35 -0400

    oky

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,24 @@
 class BirdsController < ApplicationController
 
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
+  def update
+    # find the bird we're trying to update
+    bird = Bird.find_by(id: params[:id])
+    # update the bird, using the data from the body
+    bird.update(bird_params)
+    # send a response with the updated bird
+    render json: bird
+  end
+
   # GET /birds
   def index
     birds = Bird.all
@@ -25,7 +44,7 @@ class BirdsController < ApplicationController
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
